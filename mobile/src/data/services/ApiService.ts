39484c1347a09736bafcc76a52ba45c2c886141b
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2021-06-19 00:02:41 -0300

    Homepage, search page

--- mobile/src/data/services/ApiService.ts
+++ mobile/src/data/services/ApiService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = 'https://ediaristas-workshop.herokuapp.com';
-// const url = 'http://127.0.0.1:8000/';
+// const url = 'https://ediaristas-workshop.herokuapp.com';
+const url = 'http://127.0.0.1:8000/';
 
 export const ApiService = axios.create({
   baseURL: url,
